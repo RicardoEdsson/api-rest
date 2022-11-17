5deaeef78e995e6c2f9f8849da4c6434d92d97c3
RicardoEdsson <ricardo_edsson@outlook.es>
2022-11-17 03:50:33 -0600

    first commit

---
+++ src/test/java/com/example/demo/PruebaApi1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PruebaApi1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
